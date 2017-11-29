570c0019cf138118816a0a6f05cd26e1205807aa
victory622 <yukai.chen@polito.it>
2017-11-29 18:38:09 +0100

    Update Assignment1.m

--- MatlabCodes/Assignment1.m
+++ MatlabCodes/Assignment1.m
@@ -1,7 +1,9 @@
+
+
 clear;
 im_data720mA;
 im_data1440mA;
 calculate_r_voc;
 Coeff_voc;
 Coeff_r;
-% clear x1440mA  x720mA y1440mA y720mA i step SOC R Vb1440 Vb720 Voc;
+clear x1440mA  x720mA y1440mA y720mA i step SOC R Vb1440 Vb720 Voc;
